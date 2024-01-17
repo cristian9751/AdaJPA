f9c6bd206740153302b25ff776e1ade1f50af910
CristianCasa <cristianpopicaonciu@gmail.com>
2024-01-17 18:02:58 +0100

    controller done

--- ADA JPA/src/main/java/com/cristian/JPASerpisFP/Domain/Controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.cristian.JPASerpisFP.Domain.Controller;
-
-import java.util.List;
-
-public interface Controller<T> {
-	public boolean save(T object);
-	public boolean delete(T object);
-	public List<T> getAll();
-}
