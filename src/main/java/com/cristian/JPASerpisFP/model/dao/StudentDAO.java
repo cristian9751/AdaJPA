c234edf4c4581cbe33dd2a2d8cfcd19be0fea2e6
Cristian <105856280+cristian9751@users.noreply.github.com>
2024-01-19 00:32:58 +0100

    Update StudentDAO.java

--- src/main/java/com/cristian/JPASerpisFP/model/dao/StudentDAO.java
+++ src/main/java/com/cristian/JPASerpisFP/model/dao/StudentDAO.java
@@ -1,11 +1,9 @@
 package com.cristian.JPASerpisFP.model.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import com.cristian.JPASerpisFP.Domain.Entity.Student;
 
@@ -22,18 +20,17 @@ public class StudentDAO implements IDao<Student> {
 	}
 
 	@Override
-	public List<Student> findAll() throws Exception {
+	public List<Student> findAll() {
 		EntityManager entityManager = getEntityManager();
-		Query query = entityManager.createQuery("SELECT student FROM Student student");
+		TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s", Student.class);
 		return query.getResultList();
+		
 	}
 
-	
-
 	@Override
-	public Student findById(Object studentId) throws Exception {
+	public Student findById(Object NIA) {
 		EntityManager entityManager = getEntityManager();
-		return entityManager.find(Student.class, (String) studentId);
+		return entityManager.find(Student.class, (String) NIA);
 	}
 
 	@Override
@@ -43,15 +40,12 @@ public class StudentDAO implements IDao<Student> {
 		});
 		
 	}
-	
-	 
-	public List<Student> findByName(String name) {
+
+	@Override
+	public Long countRegisters() {
 		EntityManager entityManager = getEntityManager();
-		Query query = entityManager.createQuery("SELECT s FROM Students s WHERE s.NOMBRE = :name");
-		return query.getResultList();
+		TypedQuery<Long> query = entityManager.createQuery("SELECT COUNT(s) FROM Student s", Long.class);
+		return query.getSingleResult();
 	}
 	
-	
-	
-
 }
