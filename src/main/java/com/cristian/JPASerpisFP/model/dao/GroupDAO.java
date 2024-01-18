e2db6388a1da71d162eb32dbc98e98db69228289
Cristian <105856280+cristian9751@users.noreply.github.com>
2024-01-19 00:30:23 +0100

    Update GroupDAO.java

--- src/main/java/com/cristian/JPASerpisFP/model/dao/GroupDAO.java
+++ src/main/java/com/cristian/JPASerpisFP/model/dao/GroupDAO.java
@@ -1,48 +1,53 @@
 package com.cristian.JPASerpisFP.model.dao;
 
-import com.cristian.JPASerpisFP.Domain.Entity.Group;
-
-import static com.cristian.JPASerpisFP.model.PersistenceManager.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.cristian.JPASerpisFP.Domain.Entity.Group;
+import static com.cristian.JPASerpisFP.model.PersistenceManager.*;
 
 public class GroupDAO implements IDao<Group> {
+
 	@Override
-	public  void save(Group group) throws Exception {
+	public void save(Group group) throws Exception {
 		performTransaction(entityManager -> {
 			entityManager.persist(group);
 		});
-	}
-
-	@Override
-	public List<Group> findAll() throws Exception {
-		EntityManager em = getEntityManager();
-		Query query = em.createQuery("SELECT group FROM Group group");
-		return query.getResultList();
 		
 	}
 
 	@Override
-	public Group findById(Object groupCode) throws Exception {
+	public List<Group> findAll() {
+		EntityManager entityManager = getEntityManager();
+		TypedQuery<Group> query = entityManager.createQuery("SELECT  g FROM Group g", Group.class);
+		return query.getResultList();
+	}
+
+	@Override
+	public Group findById(Object groupCode) {
 		EntityManager entityManager = getEntityManager();
 		return entityManager.find(Group.class, (int) groupCode);
-		
-		
 	}
 
 	@Override
 	public void delete(Group group) throws Exception {
 		performTransaction(entityManager -> {
-			entityManager.remove(group);
+			Group managedGroup = entityManager.merge(group);
+			entityManager.remove(managedGroup);
 		});
 		
 	}
-	
 
+	@Override
+	public Long countRegisters() {
+		EntityManager entityManager = getEntityManager();
+		TypedQuery<Long> query = entityManager.createQuery("SELECT COUNT(g) FROM Group g", Long.class);
+		return query.getSingleResult();
+	}
 
-	
 }
