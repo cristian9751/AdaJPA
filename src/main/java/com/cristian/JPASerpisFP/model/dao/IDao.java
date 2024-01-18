8647f1715127bfa1e9cb4dbef7b43d8b47e8c69f
Cristian <105856280+cristian9751@users.noreply.github.com>
2024-01-19 00:31:40 +0100

    Update IDao.java

--- src/main/java/com/cristian/JPASerpisFP/model/dao/IDao.java
+++ src/main/java/com/cristian/JPASerpisFP/model/dao/IDao.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public interface IDao<T> {
 	public void save(T object) throws Exception;
-	public List<T> findAll() throws Exception;
-	public T findById(Object object) throws Exception;
+	public List<T> findAll();
+	public T findById(Object object);
 	public void delete(T object) throws Exception;
+	public Long countRegisters();
 }
