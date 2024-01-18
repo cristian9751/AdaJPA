a98e0aba88eb0e31ab12ba5338973995448fdcc1
Cristian <105856280+cristian9751@users.noreply.github.com>
2024-01-19 00:25:10 +0100

    Update and rename Controller.java to IController.java

---
+++ src/main/java/com/cristian/JPASerpisFP/Domain/Controller/IController.java
@@ -0,0 +1,24 @@
+package com.cristian.JPASerpisFP.Domain.Controller;
+
+import com.cristian.JPASerpisFP.model.dao.IDao;
+
+import java.util.List;
+
+import com.cristian.JPASerpisFP.Domain.Controller.Utils.Enums.OperationResult;
+import com.cristian.JPASerpisFP.Domain.Entity.Group;
+
+public interface IController<T> {
+
+	public  List<T> getAll();
+	
+	public  OperationResult save(T object);
+	
+	public  T getById(Object object);
+	
+	public  OperationResult delete(Object object);
+	
+	public Long getTotal();
+	
+	
+}
+
