5cbbe3f80cc5636cf5dc43ded865f62abd404944
Cristian <105856280+cristian9751@users.noreply.github.com>
2024-01-19 00:24:18 +0100

    Update GroupController.java

--- src/main/java/com/cristian/JPASerpisFP/Domain/Controller/GroupController.java
+++ src/main/java/com/cristian/JPASerpisFP/Domain/Controller/GroupController.java
@@ -3,28 +3,22 @@ package com.cristian.JPASerpisFP.Domain.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
-
 import com.cristian.JPASerpisFP.Domain.Controller.Utils.Enums.OperationResult;
 import com.cristian.JPASerpisFP.Domain.Entity.Group;
 import com.cristian.JPASerpisFP.model.dao.IDao;
+import com.cristian.JPASerpisFP.model.dao.GroupDAO;
 
-public class GroupController extends Controller<Group> {
-
-	public GroupController(IDao<Group> dao) {
-		super(dao);
+public class GroupController implements IController<Group> {
+	
+	private static IDao<Group> dao;
+	
+	public GroupController() {
+		dao = new GroupDAO();
 	}
-
 	@Override
 	public List<Group> getAll() {
-		List<Group> groups = new ArrayList<Group>();
-		try {
-			groups = dao.findAll();
-		} catch(Exception e) {
-			System.out.println("Error: No se ha podido obtener el listado de grupos");
-		}
-		
+		List<Group> groups = new ArrayList<>();
+		groups = dao.findAll();
 		return groups;
 	}
 
@@ -33,23 +27,38 @@ public class GroupController extends Controller<Group> {
 		try {
 			dao.save(group);
 			return OperationResult.OK;
-		} catch(EntityExistsException e) {
-			return OperationResult.ALREADY_EXISTS;
 		} catch(Exception e) {
-			return OperationResult.COMMON_ERROR;
+			return OperationResult.ALREADY_EXISTS;
 		}
 	}
 
 	@Override
-	public OperationResult delete(Group group) {
+	public Group getById(Object groupCode) {
+		Group group = dao.findById(groupCode);
+		return group;
+	}
+	@Override
+	public OperationResult delete(Object groupCode) {
+		Group group = getById(groupCode);
+		if(group == null) {
+			return OperationResult.NOT_EXISTS;
+		}
+		
 		try {
 			dao.delete(group);
 			return OperationResult.OK;
-		} catch(EntityNotFoundException e) {
-			return OperationResult.NOT_EXISTS;
 		} catch(Exception e) {
+			System.out.println(e.getMessage());
 			return OperationResult.COMMON_ERROR;
 		}
+		
+		
 	}
+	
+	
+	public Long getTotal() {
+		return dao.countRegisters();
+	}
+	
 
 }
